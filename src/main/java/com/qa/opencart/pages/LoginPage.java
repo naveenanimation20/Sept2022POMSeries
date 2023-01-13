737ddf531c29db4cdd8c653cf1b3b6344a4bd40a
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-01-13 08:04:17 +0530

    updated loginpagw with locator

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -20,7 +20,6 @@ public class LoginPage {
 	private By loginBtn = By.xpath("//input[@value='Login']");
 	private By forgotPwdLink = By.linkText("Forgotten Password");
 	private By registerLink = By.linkText("Register");
-	private By myLink = By.linkText("myLink");
 
 
 	// 2. page constructor:
