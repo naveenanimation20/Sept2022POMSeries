0ea6a9d59b79f661c1c3c41ae43e3a5f3801ce27
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-01-16 07:34:58 +0530

    updated the code as per naveen review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,10 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
-	
-	
-	
-	String cart = "macbook";
-	
+
+	By cart = By.id("macbook");
+
+	public CartPage() {
+
+	}
+
+	public void addToCart() {
+
+	}
 
 }
