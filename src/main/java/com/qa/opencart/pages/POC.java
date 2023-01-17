2cd7627612877f67db5cc288fe0646e7cd0dcd03
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-01-17 07:28:45 +0530

    Create POC.java

---
+++ src/main/java/com/qa/opencart/pages/POC.java
@@ -0,0 +1,19 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class POC {
+
+	By cart = By.id("poc");
+
+	public POC() {
+
+	}
+
+	public void pocmethod() {
+		
+		System.out.println("poc is done");
+
+	}
+
+}
