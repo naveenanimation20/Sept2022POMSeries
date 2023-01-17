18e567ed9d8307a686ea0cfb865255d7543e50cd
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-01-17 07:45:25 +0530

    Update CheckoutPage.java

--- src/main/java/com/qa/opencart/pages/CheckoutPage.java
+++ src/main/java/com/qa/opencart/pages/CheckoutPage.java
@@ -3,7 +3,7 @@ package com.qa.opencart.pages;
 public class CheckoutPage {
 
 	public void checkout() {
-		System.out.println("checkout---Naveen");
+		System.out.println("checkout---Mukta");
 		int i = 10;
 	}
 
